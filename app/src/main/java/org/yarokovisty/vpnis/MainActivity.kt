553ab63d08fb22427d6d6e55264cd5d23853272d
yarokovisty <yarokovisty@gmail.com>
2026-07-04 00:35:37 +0700

    init project

---
+++ app/src/main/java/org/yarokovisty/vpnis/MainActivity.kt
@@ -0,0 +1,30 @@
+package org.yarokovisty.vpnis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.yarokovisty.vpnis.ui.theme.VPNisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VPNisTheme {
+            }
+        }
+    }
+}
